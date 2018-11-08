6b0ea1fc03c1361029362ca81d63de97a3c2c9d3
whYuuu <yjc037@gmail.com>
2018-11-08 22:51:14 +0900

    reUpdate stylesheet

--- models/User.js
+++ models/User.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
+var bcrypt   = require("bcrypt-nodejs");
 
-var bcrypt = require('bcrypt-nodejs');
 var userSchema = mongoose.Schema({
-  email:{type:String, required:true, unique:true},
-  nickname:{type:String, required:true, unique:true},
-  password:{type:String, required:true},
-  createdAt:{type:Date, default:Date.now}
+  email: {type:String, required:true, unique:true},
+  nickname: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
 });
-userSchema.pre("save",function(next){
+
+userSchema.pre("save", function(next){
   var user = this;
   if(!user.isModified("password")){
     return next();
@@ -16,13 +17,20 @@ userSchema.pre("save",function(next){
     return next();
   }
 });
-userSchema.methods.authenticate = function(password){
+
+userSchema.methods.authenticate = function (password) {
   var user = this;
   return bcrypt.compareSync(password,user.password);
 };
-userSchema.methods.hash = function(password){
-  return bcrypt.hashSync(password);
-};
-var User = mongoose.model('user',userSchema);
 
+userSchema.methods.mapObject = function (target) {
+  var exceptions = ["createdAt","_id","__v"];
+  for(var key in this.schema.paths){
+    if(exceptions.indexOf(key)<0 && target[key]){
+      this[key] = target[key];
+    }
+  }
+};
+
+var User = mongoose.model('user',userSchema);
 module.exports = User;
--- routes/home.js
+++ routes/home.js
@@ -1,33 +1,29 @@
-var express = require('express');
+var express  = require('express');
+var router   = express.Router();
 var mongoose = require('mongoose');
-var router = express.Router();
 var passport = require('../config/passport.js');
 
-router.get('/',function(req,res){
+router.get('/', function (req,res) {
   res.redirect('/posts');
 });
-router.get('/login',function(req,res){
-  res.render('login/login',{email:req.flash("email")[0],loginError:req.flash('loginError')});
+
+router.get('/login', function (req,res) {
+  res.render('login/login', {
+    email:req.flash("email")[0],
+    loginError:req.flash('loginError'),
+    loginMessage:req.flash('loginMessage')
+  });
 });
-router.post('/login',
-  function(req,res,next){
-    req.flash("email");
-    if(req.body.email.length === 0 || req.body.password.length === 0 ){
-      req.flash("email",req.body.email);
-      req.flash("loginError","please Enter both email and password");
-      req.redirect('/login');
-    }
-    else{
-      next();
-    }
-  }, passport.authenticate('local-login',{
-    successRedirect:'/posts',
-    failureRedirect:'/login',
-    failureFlash:true
-  })
-);
-router.get('/logout',function(req,res){
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect : '/posts',
+  failureRedirect : '/login',
+  failureFlash : true
+}));
+
+router.get('/logout', function(req, res) {
   req.logout();
+  req.flash("postsMessage", "Good-bye, have a nice day!");
   res.redirect('/');
 });
 
